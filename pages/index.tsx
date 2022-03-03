6efe76ca1a5e89395d7eed20dbfe51b33db938bd
Bruce Wilcoxon <brucewilcoxon@twigi-bi.io>
2022-03-03 14:57:38 -0600

    Homepage, fixed img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next'
 import styles from 'styles/Page.module.css'
+import {Box, Heading, Text} from "@chakra-ui/react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-        Yolo World
-    </div>
+      <Box className={styles.container}>
+          <Box
+              className={styles.centerContainerContent}
+          >
+              <Heading as='h2' size='xl'>
+                  Home page to come 🙃
+              </Heading>
+              <Text maxW='600px' textAlign='center' mt='1rem'>
+                  I mean probably... go checkout our portfolio in the meantime 🤗
+              </Text>
+          </Box>
+      </Box>
   )
 }
 
